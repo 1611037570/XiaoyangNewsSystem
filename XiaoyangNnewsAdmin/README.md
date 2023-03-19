7df275cab44e0765ce0f680acbdbf89f1001a141
xiaoyang <1611037570@qq.com>
2023-03-19 20:41:44 +0800

    测试提交

--- XiaoyangNnewsAdmin/README.md
+++ XiaoyangNnewsAdmin/README.md
@@ -1,4 +1,4 @@
-# xiaoyang-news-admin
+# test
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- XiaoyangNnewsAdmin/vite.config.ts
+++ XiaoyangNnewsAdmin/vite.config.ts
@@ -6,6 +6,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 服务器配置
+  server: {
+    host: '127.0.0.1',
+    port: 80,
+    open: true
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
