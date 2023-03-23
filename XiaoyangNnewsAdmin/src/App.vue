6fad25ec8142998fbbf486819e8b1aab922dca9e
xiaoyang <1611037570@qq.com>
2023-03-24 02:23:03 +0800

    1

--- XiaoyangNnewsAdmin/src/App.vue
+++ XiaoyangNnewsAdmin/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <router-view></router-view>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+console.log("1 :>> ", 1)
+</script>
 
 <style lang="less" scoped>
 * {
