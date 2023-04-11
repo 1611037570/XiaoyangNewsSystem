08db8f8408e85d842df9e92d0ac01911c9d059c7
小羊 <77526373+1611037570@users.noreply.github.com>
2023-04-12 06:31:45 +0800

    项目重构中
    项目重构中

---
+++ newsApp/src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="content">
+    <van-button type="primary" />
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+const title = ref("Hello")
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
