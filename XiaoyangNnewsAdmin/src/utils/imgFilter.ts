d965e0e45597d01a0d3be0fdc5ca5e6490322890
小羊 <77526373+1611037570@users.noreply.github.com>
2023-04-12 06:36:35 +0800

    Delete XiaoyangNnewsAdmin directory

--- XiaoyangNnewsAdmin/src/utils/imgFilter.ts
+++
@@ -1,9 +0,0 @@
-import { img } from "@/config/config.js"
-export const imgFilter = (str: any) => {
-  let data
-  str.replace(/<img [^>]*src=['"]([^'"]+)[^>]*>/, function (match: any, capture: any) {
-    data = capture
-  })
-  if (data) return data
-  else return img
-}
